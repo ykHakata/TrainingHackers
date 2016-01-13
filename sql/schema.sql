9fb8d8c09e4381c64aee2100837703de430b3641
kazhiramatsu <kazhiramatsu.github@gmail.com>
2016-01-13 22:56:27 +0900

    Add database schema

---
+++ sql/schema.sql
@@ -0,0 +1,58 @@
+drop table if exists users;
+create table users (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` VARCHAR(50) BINARY NOT NULL,
+    `username` VARCHAR(50) BINARY NOT NULL,
+    `password` VARCHAR(128) BINARY NOT NULL,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    INDEX `user_id_and_passwrd` (user_id, password)
+);
+
+drop table if exists questions;
+create table questions (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `question` TEXT BINARY NOT NULL,
+    `answer` TEXT BINARY NOT NULL,
+    `score` INT UNSIGNED NOT NULL,
+    `level` INT UNSIGNED NOT NULL,
+    `hint1` TEXT BINARY,
+    `hint2` TEXT BINARY,
+    `hint3` TEXT BINARY,
+    `hint4` TEXT BINARY,
+    `hint5` TEXT BINARY,
+    `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+drop table if exists answers;
+create table answers (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `question_id` INT UNSIGNED NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    `user_answer` TEXT BINARY NOT NULL,
+    `score` INT UNSIGNED NOT NULL,
+    `hint1` INT UNSIGNED,
+    `hint2` INT UNSIGNED,
+    `hint3` INT UNSIGNED,
+    `hint4` INT UNSIGNED,
+    `hint5` INT UNSIGNED,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX `question_id_and_user_id` (question_id, user_id)
+);
+
+drop table if exists scores;
+create table scores (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user_id` INT UNSIGNED NOT NULL,
+    `score` INT UNSIGNED NOT NULL,
+    `updated_at` TIMESTAMP  NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    INDEX `user_id` (user_id)
+);
+
+drop table if exists sessions;
+create table sessions (
+    `session_id` VARCHAR(128) BINARY NOT NULL,
+    `session_data` TEXT BINARY NOT NULL,
+    `session_expires` DATETIME NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
