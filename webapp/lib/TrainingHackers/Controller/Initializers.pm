b6e74c4c1319ccaa584ed709883f0301ab407847
kazhiramatsu <kazhiramatsu.github@gmail.com>
2016-01-18 19:37:17 +0900

    Update ceasar cipher string

--- webapp/lib/TrainingHackers/Controller/Initializers.pm
+++ webapp/lib/TrainingHackers/Controller/Initializers.pm
@@ -9,15 +9,15 @@ sub index {
  
     my $data = [
         {
-            question => 'シーザー暗号を解読せよ<br>sofifkgk',
-            answer => '1',
+            question => '以下の暗号を解読せよ<br>Vwdb Kxqjub',
+            answer => 'Stay Hungry',
             score => 10,
             level => 1,
             hint1 => 'シーザー暗号を知っているかい?',
             hint2 => 'Webでシーザー暗号を調べよう',
             hint3 => 'シーザー暗号はアルファベットを特定の文字数ずらすんだ',
-            hint4 => '',
-            hint5 => '',
+            hint4 => '問題の暗号文をアルファベット順に後ろに3文字ずらしてみよう',
+            hint5 => 'aならd、bならe、cならfに変換してみよう',
         }, 
         {
             question => 'パスワードを突破せよ',
